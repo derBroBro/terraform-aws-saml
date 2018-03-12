8c524f57696a3b2d5774428199ccf73a4611643c
Malte Brodersen <malte.brodersen@zoi.de>
2018-03-12 20:26:23 +0100

    fix os dependend cr nl

--- iam_providers.tf
+++ iam_providers.tf
@@ -1,4 +1,4 @@
 resource "aws_iam_saml_provider" "saml" {
   name                   = "${var.provider_name}"
-  saml_metadata_document = "${file(var.provider_metadata_file)}"
+  saml_metadata_document = "${replace(file(var.provider_metadata_file),"/\\r/",""}"
 }
